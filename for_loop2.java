f565a8c0977706dcfe3df935255f65939bc84a1b
Thiru352007 <thiru3.5.2007@gmail.com>
2025-11-09 15:17:57 +0530

    commit the file

---
+++ for_loop2.java
@@ -0,0 +1,18 @@
+//code to print all even number in given number
+import java.util.*;
+public class for_loop2{
+    public static void main(String[]args)
+    {
+        int n,i;
+        Scanner Sc = new Scanner (System.in);
+        System.out.println("Enter the number:");
+        n = Sc.nextInt();
+        for(i=2;i<=n;i+=2)
+
+        System.out.println("all even no is:"+i);
+        
+        
+    } 
+    
+}
+
