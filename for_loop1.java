02dae4cb628301348584850f811ff6d37ee1fbd3
Thiru352007 <thiru3.5.2007@gmail.com>
2025-11-09 15:02:18 +0530

    commiyt the file forloop1

---
+++ for_loop1.java
@@ -0,0 +1,17 @@
+//code to print reverse order in given value
+import java.util.*;
+public class for_loop1 {
+    public static void main(String[]args)
+    {
+        int n,i;
+        Scanner Sc = new Scanner (System.in);
+        System.out.println("Enter the number:");
+        n = Sc.nextInt();
+        for(i=n;i>=0;i--)
+
+        System.out.println("Reverse:"+i);
+        
+        
+    } 
+    
+}
